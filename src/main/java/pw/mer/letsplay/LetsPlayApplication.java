88d86f92fa240c9bc7021bd989e651e70fea26dd
Maksim Mikhailov <max@mer.pw>
2023-09-15 14:27:30 +0300

    Add `Product` model
    Add all the necessary fields and define json properties
    
    Add ProductRepo to connect to the mongodb database
    
    Implement a simple `GET /products` to get all products
    
    Temporarily disable authentication

--- src/main/java/pw/mer/letsplay/LetsPlayApplication.java
+++ src/main/java/pw/mer/letsplay/LetsPlayApplication.java
@@ -1,13 +1,32 @@
 package pw.mer.letsplay;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pw.mer.letsplay.model.Product;
+import pw.mer.letsplay.repository.ProductRepo;
 
 @SpringBootApplication
-public class LetsPlayApplication {
+public class LetsPlayApplication implements CommandLineRunner {
+    ProductRepo productRepo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LetsPlayApplication.class, args);
-	}
+    @Autowired
+    public void setProductRepo(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(LetsPlayApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        productRepo.save(new Product(
+                "New Product",
+                "Description",
+                100.0,
+                "user"
+        ));
+    }
 }
