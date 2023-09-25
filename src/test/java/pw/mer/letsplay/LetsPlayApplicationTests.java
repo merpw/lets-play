cc41906c549c53316c59a41d53aac8b60b2328b5
Maksim Mikhailov <max@mer.pw>
2023-09-25 17:34:34 +0300

    Add tests configuration
    Add and configure testcontainers to execute tests with a test dockerized MongoDB
    
    Exclude `test` task in Dockerfile as Docker is not available during image building
    
    Add a separate `test` job to the GH Actions workflow to run tests before building docker image
    
    Update README

--- src/test/java/pw/mer/letsplay/LetsPlayApplicationTests.java
+++ src/test/java/pw/mer/letsplay/LetsPlayApplicationTests.java
@@ -1,8 +1,48 @@
 package pw.mer.letsplay;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+import static io.restassured.RestAssured.when;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class LetsPlayApplicationTests {
-    // TODO: add tests with testcontainers
+    static MongoDBContainer mongoDB = new MongoDBContainer("mongo:latest");
+
+    @BeforeAll
+    static void beforeAll() {
+        mongoDB.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mongoDB.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.port", mongoDB::getFirstMappedPort);
+    }
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void productsShouldBePublic() {
+        when().get("/products").then().statusCode(200);
+    }
 }
