ec33b9aef798060b88f978be963621239335848a
Maksim Mikhailov <max@mer.pw>
2023-10-02 18:59:39 +0300

    Refactor controllers
    Move them to web subpackage
    
    Optimize imports

--- src/main/java/pw/mer/letsplay/web/UserController.java
+++ src/main/java/pw/mer/letsplay/web/UserController.java
@@ -1,17 +1,17 @@
-package pw.mer.letsplay;
+package pw.mer.letsplay.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pw.mer.letsplay.model.Product;
 import pw.mer.letsplay.model.User;
-import pw.mer.letsplay.repository.ProductRepo;
 import pw.mer.letsplay.repository.UserRepo;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserRepo userRepo;
 
@@ -20,12 +20,12 @@ public class UserController {
         this.userRepo = productRepo;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> index() {
         return userRepo.findAll();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public User show(@PathVariable String id) {
         return userRepo.findById(id).orElse(null);
     }
