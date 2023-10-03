d537726a1fd00359f5438bc4a962f9893fba96e3
Maksim Mikhailov <max@mer.pw>
2023-10-03 17:16:53 +0300

    Make `productRepo` private
    Keep it only in AbstractControllerTests to clean products between tests
    
    Use black-box test methodology, call `POST /products/add` instead of `productRepo.save`

--- src/test/java/pw/mer/letsplay/ProductControllerTests.java
+++ src/test/java/pw/mer/letsplay/ProductControllerTests.java
@@ -1,35 +1,37 @@
 package pw.mer.letsplay;
 
 import org.junit.jupiter.api.Test;
-import pw.mer.letsplay.model.ERole;
-import pw.mer.letsplay.model.Product;
-import pw.mer.letsplay.model.User;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static java.net.HttpURLConnection.HTTP_OK;
 import static org.hamcrest.Matchers.is;
 
 public class ProductControllerTests extends AbstractControllerTests {
     @Test
     public void productsShouldBePublic() {
-        when().get("/products").then().statusCode(200);
+        when().get("/products").then().statusCode(HTTP_OK);
     }
 
     @Test
-    public void products() {
-        when().get("/products").then().statusCode(200).body("$.size()", is(0));
+    public void adminAddsProduct() {
+        String adminToken = getAdminToken();
 
-        var user = new User("test", "test@email.com", "password", ERole.ADMIN);
-        userRepo.save(user);
+        when().get("/products").then().statusCode(HTTP_OK).body("$.size()", is(0));
 
-        var product = new Product("Test name", "Test description", 100.0, user.getId());
-        productRepo.save(product);
+        given().auth().oauth2(adminToken)
+                .contentType("application/json")
+                .body("""
+                        {
+                            "name": "Test name",
+                            "description": "Test description",
+                            "price": 100.0
+                        }
+                        """)
+                .post("/products/add")
+                .then()
+                .statusCode(HTTP_OK);
 
-        when().get("/products").then()
-                .statusCode(200)
-                .body("[0].name", is(product.getName()),
-                        "[0].description", is(product.getDescription()),
-                        "[0].price", is(product.getPrice().floatValue()),
-                        "[0].userId", is(user.getId())
-                );
+        when().get("/products").then().statusCode(HTTP_OK).body("$.size()", is(1));
     }
 }
