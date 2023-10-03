28f9d03912e78307d9955a0f0cfe841f4e308e70
Maksim Mikhailov <max@mer.pw>
2023-10-03 16:03:15 +0300

    Add `/auth/register`

--- src/test/java/pw/mer/letsplay/AuthControllerTests.java
+++ src/test/java/pw/mer/letsplay/AuthControllerTests.java
@@ -5,7 +5,7 @@ import pw.mer.letsplay.model.ERole;
 import pw.mer.letsplay.model.User;
 
 import static io.restassured.RestAssured.given;
-import static java.lang.String.format;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static pw.mer.letsplay.auth.WebSecurityConfig.passwordEncoder;
 
@@ -15,7 +15,13 @@ public class AuthControllerTests extends AbstractControllerTests {
         return given()
                 .when()
                 .contentType("application/json")
-                .body(format("{\"email\": \"%s\", \"password\": \"%s\"}", email, password))
+                .body("""
+                        {
+                            "email": "%s",
+                            "password": "%s"
+                        }
+                        """
+                        .formatted(email, password))
                 .and()
                 .post("/auth/login")
                 .then()
@@ -30,5 +36,52 @@ public class AuthControllerTests extends AbstractControllerTests {
         userRepo.save(user);
 
         getAccessToken(user.getEmail(), password);
+    }
+
+    @Test
+    void registerValid() {
+        var name = "test";
+        var email = "test@mer.pw";
+        var password = "test";
+
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                          {
+                              "name": "%s",
+                              "email": "%s",
+                              "password": "%s"
+                          }
+                        """.formatted(name, email, password))
+                .and()
+                .post("/auth/register")
+                .then()
+                .statusCode(HTTP_OK);
+
+        var user = userRepo.findByEmail(email).stream().findFirst().orElseThrow();
+        assert user.getName().equals(name);
+        assert user.getEmail().equals(email);
+    }
+
+    @Test
+    void registerEmailExists() {
+        var user = new User("test", "test@mer.pw", "test", ERole.USER);
+        userRepo.save(user);
+
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                          {
+                              "name": "test",
+                              "email": "%s",
+                              "password": "test"
+                          }
+                        """.formatted(user.getEmail()))
+                .and()
+                .post("/auth/register")
+                .then()
+                .statusCode(HTTP_BAD_REQUEST);
     }
 }
