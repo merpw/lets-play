4309a669621015a77c0c80c2eecb1e45d557393d
Maksim Mikhailov <max@mer.pw>
2023-10-03 17:51:29 +0300

    Make `userRepo` private
    Replace `userRepo.save` with new `TestUser.register`

--- src/test/java/pw/mer/letsplay/UserControllerTests.java
+++ src/test/java/pw/mer/letsplay/UserControllerTests.java
@@ -1,15 +1,13 @@
 package pw.mer.letsplay;
 
 import org.junit.jupiter.api.Test;
-import pw.mer.letsplay.model.ERole;
-import pw.mer.letsplay.model.User;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static java.net.HttpURLConnection.*;
 import static org.hamcrest.Matchers.is;
+import static pw.mer.letsplay.AuthControllerTests.TestUser;
 import static pw.mer.letsplay.AuthControllerTests.getAccessToken;
-import static pw.mer.letsplay.auth.WebSecurityConfig.passwordEncoder;
 
 public class UserControllerTests extends AbstractControllerTests {
     @Test
@@ -19,27 +17,21 @@ public class UserControllerTests extends AbstractControllerTests {
 
     @Test
     void usersShouldBeAccessibleForAdmin() {
-        var password = "test";
-        var user = new User("admin", "admin@example.com", passwordEncoder().encode(password), ERole.ADMIN);
-        userRepo.save(user);
+        String adminToken = getAdminToken();
 
-        String token = getAccessToken(user.getEmail(), password);
-
-        given().auth().oauth2(token)
+        given().auth().oauth2(adminToken)
                 .when()
                 .get("/users")
                 .then()
                 .statusCode(HTTP_OK)
-                .body("[0].id", is(user.getId()));
+                .body("$.size()", is(1));
     }
 
     @Test
     void usersShouldNotBeAccessibleForBasicUser() {
-        var password = "test";
-        var user = new User("test", "test@example.com", passwordEncoder().encode(password), ERole.USER);
-        userRepo.save(user);
-
-        String token = getAccessToken(user.getEmail(), password);
+        var testUser = new TestUser();
+        testUser.register();
+        String token = getAccessToken(testUser.email, testUser.password);
 
         given().auth().oauth2(token)
                 .when()
@@ -68,6 +60,12 @@ public class UserControllerTests extends AbstractControllerTests {
                 .then()
                 .statusCode(HTTP_OK);
 
-        assert userRepo.findByEmail("testUser@mer.pw").size() == 1;
+        given().auth().oauth2(adminToken)
+                .when()
+                .get("/users")
+                .then()
+                .statusCode(HTTP_OK)
+                .body("$.size()", is(2))
+                .body("[1].email", is("testUser@mer.pw"));
     }
 }
