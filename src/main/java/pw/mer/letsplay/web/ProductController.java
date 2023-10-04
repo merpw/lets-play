1a09a4fe66353f75f2a0e885e81734a3580b64d6
Maksim Mikhailov <max@mer.pw>
2023-10-04 15:25:40 +0300

    Add CRUD operations for `/products`
    Add `products:write` scope
    
    Use `@Getter` and `@Setter` in Product

--- src/main/java/pw/mer/letsplay/web/ProductController.java
+++ src/main/java/pw/mer/letsplay/web/ProductController.java
@@ -1,12 +1,17 @@
 package pw.mer.letsplay.web;
 
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pw.mer.letsplay.model.Product;
 import pw.mer.letsplay.repository.ProductRepo;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("/products")
@@ -23,16 +28,68 @@ public class ProductController {
         return productRepo.findAll();
     }
 
-    public static class ProductAddRequest {
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable String id) {
+        return productRepo.findById(id).orElseThrow(() ->
+                new ResponseStatusException(NOT_FOUND, "Product not found"));
+    }
+
+    public static class AddProductRequest {
         public String name;
         public String description;
-        public double price;
+        public Double price;
     }
 
     @PostMapping("/add")
-    public Product add(@RequestBody ProductAddRequest request, Authentication authentication) {
+    public String add(@RequestBody AddProductRequest request, Authentication authentication) {
         var product = new Product(request.name, request.description, request.price, authentication.getName());
         productRepo.save(product);
-        return product;
+        return product.getId();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id, Authentication authentication) {
+        var product = productRepo.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Product not found"));
+
+        if (!product.getUserId().equals(authentication.getName())) {
+            throw new ResponseStatusException(FORBIDDEN, "You're not an owner of this product");
+        }
+
+        productRepo.deleteById(id);
+    }
+
+    public static class UpdateProductRequest {
+        @Nullable
+        public String name;
+        @Nullable
+        public String description;
+        @Nullable
+        public Double price;
+    }
+
+    @PutMapping("/{id}")
+    public void updateById(@PathVariable String id, @RequestBody UpdateProductRequest request, Authentication authentication) {
+
+        var product = productRepo.findById(id).orElseThrow(() ->
+                new ResponseStatusException(NOT_FOUND, "Product not found"));
+
+        if (!product.getUserId().equals(authentication.getName())) {
+            throw new ResponseStatusException(FORBIDDEN, "You're not an owner of this product");
+        }
+
+        if (request.name != null) {
+            product.setName(request.name);
+        }
+
+        if (request.description != null) {
+            product.setDescription(request.description);
+        }
+
+        if (request.price != null) {
+            product.setPrice(request.price);
+        }
+
+        productRepo.save(product);
     }
 }
