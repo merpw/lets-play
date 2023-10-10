b7ec7febee62121d50d18d1586c2d65e9039516e
Maksim Mikhailov <max@mer.pw>
2023-10-10 21:31:08 +0300

    Improve UserController validation
    Move validation annotations to a custom UserValidators class to reuse them in several handlers

--- src/main/java/pw/mer/letsplay/model/ERole.java
+++ src/main/java/pw/mer/letsplay/model/ERole.java
@@ -1,9 +1,17 @@
 package pw.mer.letsplay.model;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 import java.util.Collection;
 import java.util.List;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 public enum ERole {
     ADMIN,
@@ -20,6 +28,37 @@ public enum ERole {
             return List.of("users", "products:write");
         } else {
             return List.of();
+        }
+    }
+
+    public static ERole fromString(String role) throws IllegalArgumentException {
+        return ERole.valueOf(role.toUpperCase());
+    }
+
+    // Custom validation annotation (@ERole.Valid) to check if the string is a valid role
+    // https://www.baeldung.com/javax-validations-enums
+
+    @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+    @Retention(RUNTIME)
+    @Documented
+    @Constraint(validatedBy = Validator.class)
+    public @interface Valid {
+        String message() default "Role must be any of 'admin', 'user'";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+    }
+
+    public static class Validator implements jakarta.validation.ConstraintValidator<Valid, String> {
+        @Override
+        public boolean isValid(String value, jakarta.validation.ConstraintValidatorContext context) {
+            try {
+                ERole.fromString(value);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
         }
     }
 }
