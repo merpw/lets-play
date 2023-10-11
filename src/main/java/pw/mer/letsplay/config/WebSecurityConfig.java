55eb8a02ab9c3e5ca148c01c9a07df474e4ea9a2
Maksim Mikhailov <max@mer.pw>
2023-10-11 18:56:31 +0300

    Move jwt configuration to a separate file

--- src/main/java/pw/mer/letsplay/config/WebSecurityConfig.java
+++ src/main/java/pw/mer/letsplay/config/WebSecurityConfig.java
@@ -1,12 +1,5 @@
-package pw.mer.letsplay.auth;
+package pw.mer.letsplay.config;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,13 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -31,9 +18,8 @@ import static org.springframework.http.HttpMethod.GET;
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/error").permitAll()
@@ -48,30 +34,12 @@ public class WebSecurityConfig {
                 )
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/**"))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(
-                                jwt -> jwt.decoder(jwtDecoder())
+                                jwt -> jwt.decoder(jwtDecoder)
                         )
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
-    }
-
-    @Value("${jwt.public_key}")
-    RSAPublicKey publicKey;
-
-    @Value("${jwt.private_key}")
-    RSAPrivateKey privateKey;
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
-    }
-
-    @Bean
-    JwtEncoder jwtEncoder() {
-        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
-        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new NimbusJwtEncoder(jwkSource);
     }
 
     @Bean
