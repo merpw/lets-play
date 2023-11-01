0f0fa442d708e5944b6ba9af3b2fd08652a196cc
Adrian Lui <luinairda@gmail.com>
2023-11-01 22:11:00 +0200

    Added frontend docker setup

--- buy-01-frontend/src/environments/environment.development.ts
+++ buy-01-frontend/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   // set dev env variables here
   production: false,
-  // apiUrl: 'http://localhost:80/',
 };
--- buy-01-frontend/src/environments/environment.ts
+++ buy-01-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   // set prod env variables here
   production: true,
-  // apiUrl: 'http://production_api_url'
 };
