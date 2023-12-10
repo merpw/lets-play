72656bdae798be893768a5a3f56d20287c16f279
Maxim Mikhailov <dev@maxim.ax>
2023-12-10 19:24:52 +0200

    Fix bug in documentation
    When generated server url is not valid while running locally
    
    Add `DOCS_API_URL`, and use it instead of generated server url

--- backend/services/products/src/main/java/pw/mer/letsplay/config/SwaggerConfiguration.java
+++ backend/services/products/src/main/java/pw/mer/letsplay/config/SwaggerConfiguration.java
@@ -1,6 +1,8 @@
 package pw.mer.letsplay.config;
 
+import org.springframework.context.annotation.Configuration;
 import pw.mer.shared.config.SharedSwaggerConfiguration;
 
+@Configuration
 public class SwaggerConfiguration extends SharedSwaggerConfiguration {
 }
--- backend/shared/src/main/java/pw/mer/shared/config/SharedSwaggerConfiguration.java
+++ backend/shared/src/main/java/pw/mer/shared/config/SharedSwaggerConfiguration.java
@@ -1,13 +1,16 @@
 package pw.mer.shared.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(servers = {@Server(url = "/api")})
 @SecurityScheme(
         type = SecuritySchemeType.HTTP,
         name = "Authentication",
@@ -15,4 +18,15 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SharedSwaggerConfiguration {
+    @Value("${docs.api-url}")
+    private String apiUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        if (apiUrl == null) {
+            apiUrl = "/";
+        }
+        Server server = new Server().url(apiUrl);
+        return new OpenAPI().servers(List.of(server));
+    }
 }
